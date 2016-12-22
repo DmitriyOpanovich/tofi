8191c12147025c56be9855ee1335544bccb08bcd
Vladislav Bulikov <uladzislau.bulikau@sriinfotech.com>
2016-12-22 13:39:29 +0300

    lost files

---
+++ bots/credit-assistant-bot/src/main/java/by/bsuir/tofi/finance_assistant/bots/credit_assistant_bot/model/enums/AnyOption.java
@@ -0,0 +1,24 @@
+package by.bsuir.tofi.finance_assistant.bots.credit_assistant_bot.model.enums;
+
+
+public class AnyOption extends BaseEnumEntity {
+    @Override
+    public String getName() {
+        return "Не важно";
+    }
+
+    @Override
+    public String getEn_descr() {
+        return "Any option would be acceptable";
+    }
+
+    @Override
+    public String getRu_descr() {
+        return "Подойдет любой вариант";
+    }
+
+    @Override
+    public String getDesc(String language) {
+        return super.getDesc(language);
+    }
+}
